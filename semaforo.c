df83bfe8e9b4729052c141b9735bb12b90ac8879
BraulioP696 <bepezantes@gmail.com>
2023-04-30 16:14:12 -0500

    gpt

--- semaforo.c
+++ semaforo.c
@@ -1,60 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <alchemy/task.h>
 #include <alchemy/sem.h>
+#include <alchemy/task.h>
 
-#define NUM_THREADS 2
+RT_SEM sem; // semáforo
+int var_compartida = 0; // variable compartida
 
-int global_var = 0;
-RT_SEM sem;
-
-void taskOne(void *arg) {
-    int i;
-    for(i=0; i<1000000; i++) {
-        rt_sem_p(&sem, TM_INFINITE);
-        global_var++;
-        printf("val: %d",global_var);
-        rt_sem_v(&sem);
-    }
+void hilo_1(void *arg) {
+  int i;
+  for (i = 0; i < 10000; i++) {
+    rt_sem_p(&sem, TM_INFINITE); // bloquea el semáforo
+    var_compartida++; // incrementa la variable compartida
+    printf("val: %d \n" ,var_compartida);
+    rt_sem_v(&sem); // libera el semáforo
+  }
 }
 
-void taskTwo(void *arg) {
-    int i;
-    for(i=0; i<1000000; i++) {
-        rt_sem_p(&sem, TM_INFINITE);
-        global_var--;
-        printf("va2: %d",global_var);
-        rt_sem_v(&sem);
-    }
+void hilo_2(void *arg) {
+  int i;
+  for (i = 0; i < 10000; i++) {
+    rt_sem_p(&sem, TM_INFINITE); // bloquea el semáforo
+    var_compartida--; // decrementa la variable compartida
+    printf("val2: %d \n" ,var_compartida);
+    rt_sem_v(&sem); // libera el semáforo
+  }
 }
 
 int main(int argc, char* argv[]) {
-    RT_TASK tasks[NUM_THREADS];
-    int i;
+  rt_sem_create(&sem, "mi_sem", 1, S_FIFO); // crea el semáforo con valor inicial 1
 
-    rt_sem_create(&sem, "my_semaphore", 1, S_FIFO);
+  RT_TASK tarea_1, tarea_2;
+  rt_task_create(&tarea_1, "hilo_1", 0, 50, T_JOINABLE);
+  rt_task_create(&tarea_2, "hilo_2", 0, 50, T_JOINABLE);
 
-    for(i=0; i < NUM_THREADS; i++) {
-        if(i==0)
-            rt_task_create(&tasks[i], "taskOne", 0, 50, 0);
-        else
-            rt_task_create(&tasks[i], "taskTwo", 0, 50, 0);
-    }
+  rt_task_start(&tarea_1, &hilo_1, NULL);
+  rt_task_start(&tarea_2, &hilo_2, NULL);
 
-    for(i=0; i < NUM_THREADS; i++) {
-        if(i==0)
-            rt_task_start(&tasks[i], &taskOne, NULL);
-        else
-            rt_task_start(&tasks[i], &taskTwo, NULL);
-    }
+  rt_task_join(&tarea_1);
+  rt_task_join(&tarea_2);
 
-    for(i=0; i < NUM_THREADS; i++) {
-        rt_task_join(&tasks[i]);
-    }
+  printf("La variable compartida tiene un valor de %d\n", var_compartida);
 
-    printf("global_var: %d\n", global_var);
+  rt_sem_delete(&sem); // elimina el semáforo
 
-    rt_sem_delete(&sem);
-
-    return 0;
+  return 0;
 }
