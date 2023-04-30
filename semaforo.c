dd33d6221a28433d67efbbb6e4f6c1072fd8c4c9
BraulioP696 <bepezantes@gmail.com>
2023-04-30 15:06:55 -0500

    INCLUYE SEMAFORO

--- semaforo.c
+++ semaforo.c
@@ -15,26 +15,48 @@ static RT_TASK tarea2;
 
 int global = 0; //RECURSO COMPARTIDO
 
+//DEFINIMOS SEMAFORO
+RT_SEM semaforo;
+int sem_id;
+
 void tareaUno(void *arg){
     int i;
     for(i=0; i <ITER; i++){
+        // ESPERAMOS EL SEMAFORO 
+        rt_sem_p(&semaforo, TM_INFINITE);
+        //IMPRESION VARIABLE INCREMENTADA
         printf("Tarea 1 la variable global es: %d \n",++global);
+        //LIBERAMOS SEMAFORO
+        rt_sem_v(&semaforo);
     }
 }
 
 void tareaDos(void *arg){
     int i;
     for(i=0; i  <ITER; i++){
+        // ESPERAMOS EL SEMAFORO 
+        rt_sem_p(&semaforo, TM_INFINITE);
+        //IMPRESION VARIABLE INCREMENTADA
         printf("Tarea 2 la variable global es: %d \n",--global);
+        //LIBERAMOS SEMAFORO
+        rt_sem_v(&semaforo);
     }
 }
 
 int main(int argc, char* argv[]){ //ARGUMENTOS DE LA FUNCIÓN PRINCIPAL
+    //CREACIÓN DE SEMAFORO
+    sem_id = rt_sem_create(&semaforo, "SemaforoP3",1,S_FIFO);
+    // MENSAJE EN CASO DE ERROR CREANDO SEMAFORO
+    if(sem_id < 0) {
+        printf("Error creando semáforo\n");
+        exit(EXIT_FAILURE);
+    }
     char str1[10]; char str2[10]; //CREAMOS EL STRING (ARREGLO DE CARACTERES)
     sprintf(str1,"task_1"); sprintf(str2,"task_2");//COLOCAMOS NOMBRE A UN STRING
     rt_task_create(&tarea1, str1,0,1,0); //CREAMOS LA TAREA, DIRECCIÓN, NOMBRE DEL HILO, PRIORIDADES EN XENOMAI
     rt_task_create(&tarea2, str2,0,1,0); //CREAMOS LA TAREA, DIRECCIÓN, NOMBRE DEL HILO, PRIORIDADES EN XENOMAI
     rt_task_start(&tarea1, &tareaUno,0); //INICIAMOS LA TAREA, DIRECCIÓN TAREA, FUNCIÓN A EJECUTAR, 0
     rt_task_start(&tarea2, &tareaDos,0); //INICIAMOS LA TAREA, DIRECCIÓN TAREA, FUNCIÓN A EJECUTAR, 0
+    rt_sem_delete(&semaforo); //ELIMINAMOS EL SEMAFORO CREADO
     return 0;
 }
