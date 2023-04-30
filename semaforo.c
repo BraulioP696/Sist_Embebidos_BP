0e12d20a24f3997925a98a380d56a95006d8e65e
BraulioP696 <bepezantes@gmail.com>
2023-04-30 16:02:20 -0500

    semgpt

--- semaforo.c
+++ semaforo.c
@@ -1,66 +1,47 @@
 #include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <alchemy/task.h> //Libreria xenomai para generar tareas RT
-#include <alchemy/timer.h>
 #include <alchemy/sem.h>
+#include <alchemy/task.h>
 
-//DEFINIMOS EL NUMERO DE ITERACIONES POR TAREA
-#define ITER 100000
+#define N 10000
 
-//DEFINIMOS LAS TAREAS
-static RT_TASK tarea1, tarea2;
+int shared_variable = 0;
+RT_SEM semaphore;
 
-int global = 0; //RECURSO COMPARTIDO
-
-//DEFINIMOS SEMAFORO
-RT_SEM sem;
-int sem_id;
-
-void tareaUno(void *arg){
+void increment_thread(void *arg) {
     int i;
-    for(i=0; i <ITER; i++){
-        // ESPERAMOS EL SEMAFORO 
-        rt_sem_p(&sem, TM_INFINITE);
-        //IMPRESION VARIABLE INCREMENTADA
-        printf("Tarea 11 la variable global es: %d \n",++global);
-        //LIBERAMOS SEMAFORO
-        rt_sem_v(&sem);
+    for (i = 0; i < N; i++) {
+        rt_sem_p(&semaphore, TM_INFINITE);
+        shared_variable++;
+        rt_sem_v(&semaphore);
     }
 }
 
-
-void tareaDos(void *arg){
+void decrement_thread(void *arg) {
     int i;
-    for(i=0; i  <ITER; i++){
-        // ESPERAMOS EL SEMAFORO 
-        rt_sem_p(&sem, TM_INFINITE);
-        //IMPRESION VARIABLE INCREMENTADA
-        printf("Tarea 22 la variable global es: %d \n",--global);
-        //LIBERAMOS SEMAFORO
-        rt_sem_v(&sem);
+    for (i = 0; i < N; i++) {
+        rt_sem_p(&semaphore, TM_INFINITE);
+        shared_variable--;
+        rt_sem_v(&semaphore);
     }
 }
 
-int main(int argc, char* argv[]){ //ARGUMENTOS DE LA FUNCIÓN PRINCIPAL
-    //CREACIÓN DE SEMAFORO
-    rt_sem_create(&sem, "SemaforoP3",1,S_PRIO);
-    // MENSAJE EN CASO DE ERROR CREANDO SEMAFORO
-    if(sem_id < 0) {
-        printf("Error creando semáforo\n");
-        exit(EXIT_FAILURE);}
-    else{printf("Semaforo creado con exito\n");}
-    
-    char str1[10]; char str2[10]; //CREAMOS EL STRING (ARREGLO DE CARACTERES)
-    sprintf(str1,"task_1"); sprintf(str2,"task_2");//COLOCAMOS NOMBRE A UN STRING
-    rt_task_create(&tarea1, str1,0,1,T_JOINABLE); //CREAMOS LA TAREA, DIRECCIÓN, NOMBRE DEL HILO, PRIORIDADES EN XENOMAI
-    rt_task_create(&tarea2, str2,0,1,T_JOINABLE); //CREAMOS LA TAREA, DIRECCIÓN, NOMBRE DEL HILO, PRIORIDADES EN XENOMAI
-    rt_task_start(&tarea1, &tareaUno,NULL); //INICIAMOS LA TAREA, DIRECCIÓN TAREA, FUNCIÓN A EJECUTAR, 0
-    rt_task_start(&tarea2, &tareaDos,NULL); //INICIAMOS LA TAREA, DIRECCIÓN TAREA, FUNCIÓN A EJECUTAR, 0
-    rt_task_join(&tarea1);
-    rt_task_join(&tarea2);
-    printf("El valor finbal es: %d \n", global);
-    rt_sem_delete(&sem); //ELIMINAMOS EL SEMAFORO CREADO
+int main(int argc, char *argv[]) {
+    RT_TASK increment_task, decrement_task;
+
+    rt_sem_create(&semaphore, "my_semaphore", 1, S_FIFO);
+
+    rt_task_create(&increment_task, "increment_task", 0, 50, T_JOINABLE);
+    rt_task_create(&decrement_task, "decrement_task", 0, 50, T_JOINABLE);
+
+    rt_task_start(&increment_task, &increment_thread, NULL);
+    rt_task_start(&decrement_task, &decrement_thread, NULL);
+
+    rt_task_join(&increment_task);
+    rt_task_join(&decrement_task);
+
+    printf("The final value of shared_variable is %d\n", shared_variable);
+
+    rt_sem_delete(&semaphore);
+
     return 0;
 }
