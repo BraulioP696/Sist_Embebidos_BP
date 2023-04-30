7ff16df3a0cd549a5a80cbac5cec2340d76d61a3
BraulioP696 <bepezantes@gmail.com>
2023-04-30 12:50:08 -0500

    CAMBIO A TIEMPO 1

--- tiempo1.c
+++ tiempo1.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-#include <math>
-#include <alchemy/task.h> #
+
+#include <alchemy/task.h> //Libreria xenomai para generar tareas RT
 
 
-RT_TASK tarea_hola;
+RT_TASK tarea_hola; //CREAMOS LA ESTRUCTURA DE TAREA
 
-void mensaje (void *arg){
-    RT_TASK_INFO infoTarea;
-    printf("Hola mundo\n");
+void mensaje (void *arg){  // FUNCIÓN DE LA TAREA "MENSAJE"
+    RT_TASK_INFO infoTarea; // CREAMOS LA VARIABLE DE LA INFORMACIÓN DE LA TAREA
+    printf("Hola mundo\n"); // ACCIÓN DE LA TAREA
 
-    rt_task_inquire(NULL, &infoTarea);
-    printf("Tarea: %s \n", infoTarea.name);
-}
+    rt_task_inquire(NULL, &infoTarea); //SOLICITAMOS INFORMACIÓN DE LA TAREA QUE EJECUTA LA FUNCIÓN
+    printf("Tarea: %s \n", infoTarea.name); //IMPRIMIMOS LA INFORMACIÓN DE LA TAREA
+     // ASI IDENTIFICAMOS QUE SE EJECUTA CON LE HILO DE LA TAREA MENSAJE
+}  
 
 
-int main(int argc, char* argv[]){
-    char str[10];
-    sprintf(str,"hola");
-    rt_task_create(&tarea_hola, str,0,50,0);
-    rt_task_start(&tarea_hola, &mensaje,0);
+int main(int argc, char* argv[]){ //ARGUMENTOS DE LA FUNCIÓN PRINCIPAL
+    char str[10];  //CREAMOS EL STRING (ARREGLO DE CARACTERES)
+    sprintf(str,"hola"); //COLOCAMOS NOMBRE A UN STRING
+    rt_task_create(&tarea_hola, str,0,50,0); //CREAMOS LA TAREA, DIRECCIÓN, NOMBRE DEL HILO, PRIORIDADES EN XENOMAI
+    rt_task_start(&tarea_hola, &mensaje,0); //INICIAMOS LA TAREA, DIRECCIÓN TAREA, FUNCIÓN A EJECUTAR, 0
 }
