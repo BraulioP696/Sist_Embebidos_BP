4c206fcd40f3f0d1d73d3d4be140e193958caad9
BraulioP696 <bepezantes@gmail.com>
2023-04-30 15:31:43 -0500

    semaforo

--- semaforo.c
+++ semaforo.c
@@ -24,11 +24,11 @@ void tareaUno(void *arg){
     int i;
     for(i=0; i <ITER; i++){
         // ESPERAMOS EL SEMAFORO 
-        rt_sem_p(&semaforo, TM_INFINITE);
+        rt_sem_p(&sem, TM_INFINITE);
         //IMPRESION VARIABLE INCREMENTADA
         printf("Tarea 11 la variable global es: %d \n",++global);
         //LIBERAMOS SEMAFORO
-        rt_sem_v(&semaforo);
+        rt_sem_v(&sem);
     }
 }
 
@@ -37,17 +37,17 @@ void tareaDos(void *arg){
     int i;
     for(i=0; i  <ITER; i++){
         // ESPERAMOS EL SEMAFORO 
-        rt_sem_p(&semaforo, TM_INFINITE);
+        rt_sem_p(&sem, TM_INFINITE);
         //IMPRESION VARIABLE INCREMENTADA
         printf("Tarea 22 la variable global es: %d \n",--global);
         //LIBERAMOS SEMAFORO
-        rt_sem_v(&semaforo);
+        rt_sem_v(&sem);
     }
 }
 
 int main(int argc, char* argv[]){ //ARGUMENTOS DE LA FUNCIÓN PRINCIPAL
     //CREACIÓN DE SEMAFORO
-    sem_id = rt_sem_create(&semaforo, "SemaforoP3",1,S_PRIO);
+    sem_id = rt_sem_create(&sem, "SemaforoP3",1,S_PRIO);
     // MENSAJE EN CASO DE ERROR CREANDO SEMAFORO
     if(sem_id < 0) {
         printf("Error creando semáforo\n");
@@ -60,6 +60,6 @@ int main(int argc, char* argv[]){ //ARGUMENTOS DE LA FUNCIÓN PRINCIPAL
     rt_task_create(&tarea2, str2,0,1,0); //CREAMOS LA TAREA, DIRECCIÓN, NOMBRE DEL HILO, PRIORIDADES EN XENOMAI
     rt_task_start(&tarea1, &tareaUno,0); //INICIAMOS LA TAREA, DIRECCIÓN TAREA, FUNCIÓN A EJECUTAR, 0
     rt_task_start(&tarea2, &tareaDos,0); //INICIAMOS LA TAREA, DIRECCIÓN TAREA, FUNCIÓN A EJECUTAR, 0
-    rt_sem_delete(&semaforo); //ELIMINAMOS EL SEMAFORO CREADO
+    rt_sem_delete(&sem); //ELIMINAMOS EL SEMAFORO CREADO
     return 0;
 }
