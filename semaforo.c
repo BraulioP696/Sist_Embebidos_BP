5fa57612f231017de08e7f385a6b939002384a40
BraulioP696 <bepezantes@gmail.com>
2023-04-30 16:11:28 -0500

    semgpt

--- semaforo.c
+++ semaforo.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
-#include <alchemy/sem.h>
+#include <stdlib.h>
 #include <alchemy/task.h>
+#include <alchemy/sem.h>
 
-#define MAX_COUNT 1000000
+#define NUM_THREADS 2
 
 int global_var = 0;
 RT_SEM sem;
 
-void task1(void *arg)
-{
+void taskOne(void *arg) {
     int i;
-    for(i = 0; i < MAX_COUNT; i++)
-    {
+    for(i=0; i<1000000; i++) {
         rt_sem_p(&sem, TM_INFINITE);
         global_var++;
-        printf("val1: %d \n",global_var);
+        printf("val: %d",global_var);
         rt_sem_v(&sem);
     }
 }
 
-void task2(void *arg)
-{
+void taskTwo(void *arg) {
     int i;
-    for(i = 0; i < MAX_COUNT; i++)
-    {
+    for(i=0; i<1000000; i++) {
         rt_sem_p(&sem, TM_INFINITE);
         global_var--;
-        printf("val2: %d \n",global_var);
+        printf("va2: %d",global_var);
         rt_sem_v(&sem);
     }
 }
 
-int main(int argc, char **argv)
-{
-    rt_sem_create(&sem, "Semaphore", 1, S_FIFO);
-    
-    RT_TASK taskA, taskB;
-    rt_task_create(&taskA, "Task1", 0, 50, 0);
-    rt_task_create(&taskB, "Task2", 0, 50, 0);
-    rt_task_start(&taskA, &task1, 0);
-    rt_task_start(&taskB, &task2, 0);
-    rt_task_join(&taskA);
-    rt_task_join(&taskB);
+int main(int argc, char* argv[]) {
+    RT_TASK tasks[NUM_THREADS];
+    int i;
+
+    rt_sem_create(&sem, "my_semaphore", 1, S_FIFO);
+
+    for(i=0; i < NUM_THREADS; i++) {
+        if(i==0)
+            rt_task_create(&tasks[i], "taskOne", 0, 50, 0);
+        else
+            rt_task_create(&tasks[i], "taskTwo", 0, 50, 0);
+    }
+
+    for(i=0; i < NUM_THREADS; i++) {
+        if(i==0)
+            rt_task_start(&tasks[i], &taskOne, NULL);
+        else
+            rt_task_start(&tasks[i], &taskTwo, NULL);
+    }
+
+    for(i=0; i < NUM_THREADS; i++) {
+        rt_task_join(&tasks[i]);
+    }
+
+    printf("global_var: %d\n", global_var);
 
     rt_sem_delete(&sem);
+
     return 0;
 }
