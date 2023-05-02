6af124f1659e8d0cf4f099c1e933a298bead7c22
BraulioP696 <bepezantes@gmail.com>
2023-05-01 20:48:37 -0500

    ww

--- semaforo.c
+++ semaforo.c
@@ -36,10 +36,11 @@ void tareaDos(void *arg){
     for(i=0; i  <ITER; i++){
         // ESPERAMOS EL SEMAFORO 
         //IMPRESION VARIABLE INCREMENTADA
+        rt_sem_p(&sem, TM_NONBLOCK);
         global -=1;
         printf("Tarea 22 la variable global es: %d \n",global);
         //LIBERAMOS SEMAFORO
-        rt_sem_v(&sem)
+        rt_sem_v(&sem);
     }
 }
 
